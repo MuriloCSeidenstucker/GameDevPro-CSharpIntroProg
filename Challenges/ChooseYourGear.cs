22873b1fca0cc49f8d2f680d6752a361437cf9e0
Murilo C. Seidenstucker <murilocampos.004@gmail.com>
2024-01-08 12:02:31 -0300

    ChooseYourGear challenge completed

--- Challenges/ChooseYourGear.cs
+++ Challenges/ChooseYourGear.cs
@@ -9,8 +9,33 @@ public class ChooseYourGear
         Console.WriteLine();
 
         string[] choices = new string[3];
-        Console.WriteLine("Você se depara com os seguintes itens: 1:{} e 2:{}");
-        Console.WriteLine($"Para pegar o item, digite o número referente à sua escolha");
+        choices[0] = GetInput("PANELA", "TRAVESSEIRO");
+        choices[1] = GetInput("ARMA", "GAME BOY");
+        choices[2] = GetInput("CAMISA", "BARRA DE CHOCOLATE");
+
+        Console.WriteLine($"Suas escolhas foram: {choices[0]}, {choices[1]} e {choices[2]}");
+    }
+
+    private static string GetInput(string optionA, string optionB)
+    {
+        string value = string.Empty;
+        Console.WriteLine($"Você se depara com os seguintes itens: 1:{optionA} e 2:{optionB}");
+        Console.WriteLine($"Para pegar o item, digite o número correspondente");
+        bool success = int.TryParse(Console.ReadLine(), out var input);
+        if (success && input == 1)
+        {
+            value = optionA;
+        }
+        else if (success && input == 2)
+        {
+            value = optionB;
+        }
+        else
+        {
+            value = "Entrada de dados incorreta";
+        }
+
+        return value;
     }
 
     private static void PrintAndWait(string message)
--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using CSharpIntroProg.Challenges;
 
-RockPaperScissor.Execute();
+ChooseYourGear.Execute();
 
 Console.ReadKey();
